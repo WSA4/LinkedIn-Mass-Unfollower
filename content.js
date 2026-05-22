7afdf914889369b7b86500a0cbc0f529c7693842
Waleed <269268463+WSA4@users.noreply.github.com>
2026-05-22 14:18:22 +0300

    feat: Add Edge/Chromium polyfill to content.js

--- content.js
+++ content.js
@@ -1,4 +1,7 @@
-/**
+// Edge/Chromium compatibility polyfill (allows using `browser.*` namespace on Chrome/Edge)
+if (typeof browser === "undefined") { var browser = chrome; }
+    
+    /**
  * LinkedIn Mass Unfollower Extension (2025)
  * Developed by: Prof. Llewellyn E. van Zyl (Ph.D)
  * Website: www.psynalytics.com
